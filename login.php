c5a8cd16f1e5aa6798d0bf55d61cb68718403456
Santhosh Raj <156707748+sandyddeveloper@users.noreply.github.com>
2025-07-08 16:04:43 +0530

    Update login.php

--- login.php
+++ login.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['username'] = $user['username'];
-        header("Location: dashboard.php");
+        header("Location: ./dashboard.php");
     } else {
         echo "Invalid login!";
     }
