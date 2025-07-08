ffb923e2db860c54226c89bd6c077471e36d2460
Santhosh Raj <156707748+sandyddeveloper@users.noreply.github.com>
2025-07-08 16:06:42 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->bind_param("sss", $username, $email, $password);
 
     if ($stmt->execute()) {
-        header("Location: login.php");
+        header("Location: ./login.php");
     } else {
         echo "Signup failed!";
     }
